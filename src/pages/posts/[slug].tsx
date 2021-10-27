5127fc5be0f2291ca8fbbd34c3094ea8da76ae9f
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-26 22:11:53 -0300

    feat: validate if user has a active subscription when access any post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -41,6 +41,14 @@ export const getServerSideProps: GetServerSideProps = async ({
 }) => {
 	const session = await getSession({ req });
 
+	if (!session?.activeSubscription) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
 	const { slug } = params;
 
 	const prismic = getPrismicClient(req);
