e53cdbddf160ab950e14624395bfa8a61290d6bf
Matheus Santos Soares <mss.soares22@gmail.com>
2021-10-26 21:08:59 -0300

    fix: change menu items from a to next Link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,23 @@
-import { SignInButton } from "../SignInButton";
-import styles from "./styles.module.scss";
+import Link from 'next/link';
+import { SignInButton } from '../SignInButton';
+import styles from './styles.module.scss';
 
 export function Header() {
-  return (
-    <header className={styles.headerContainer}>
-      <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+	return (
+		<header className={styles.headerContainer}>
+			<div className={styles.headerContent}>
+				<img src="/images/logo.svg" alt="ig.news" />
 
-        <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
-        </nav>
-        <SignInButton />
-      </div>
-    </header>
-  );
+				<nav>
+					<Link href="/">
+						<a className={styles.active}>Home</a>
+					</Link>
+					<Link href="/posts" prefetch>
+						<a>Posts</a>
+					</Link>
+				</nav>
+				<SignInButton />
+			</div>
+		</header>
+	);
 }
